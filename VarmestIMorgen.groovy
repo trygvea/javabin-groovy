415a8268a3af41e2ee43eea8cd02ae51cb1447e1
trygvea <trygve.amundsen@gmail.com>
2011-11-23 23:28:20 +0100

    Initial commit

---
+++ VarmestIMorgen.groovy
@@ -0,0 +1,37 @@
+import groovy.json.JsonSlurper
+
+System.setProperty("file.encoding", "UTF-8")
+    
+FORSLAG_URL = "http://www.yr.no/_/websvc/jsonforslagsboks.aspx?s=" 
+// Gir: [["Bergen",,,,],[["Bergen","/sted/Norge/Hordaland/Bergen/Bergen/","By (Hordaland)","NO"],... (andre byer og steder i verden)
+    
+String hentVarselUrl(by) {
+    def json = new URL(FORSLAG_URL + URLEncoder.encode(by)).text
+    def forslag = new JsonSlurper().parseText(json.replace(',,,,', ',')) // Bug i JsonParser ...
+    def førsteByINorge = forslag[1].find { 
+        it[2].startsWith("By") && it[3] == 'NO'
+    }
+    'http://www.yr.no' + førsteByINorge[1] + 'varsel.xml'
+}
+
+int finnMaxTempForBy(by, url, dato) {
+    def vær = new XmlSlurper().parse(url)
+    def morgendagensVærData = vær.forecast.tabular.time.findAll {
+        it.@from.text().substring(0,10) == dato.format('yyyy-MM-dd')
+    }
+    morgendagensVærData*.temperature*.@value*.text()*.toInteger().max()
+}
+
+Map finnMaxTempPerBy(byer, dato) {
+    byer.collectEntries { by ->
+        [(by): finnMaxTempForBy(by, hentVarselUrl(by), dato)]
+    }
+}
+
+def byer = ["Oslo", "Bergen", "Trondheim", "Tromsø", "Kristiansand", "Drammen", "Stavanger", "Haugesund", "Fredrikstad"]
+def imorgen = new Date()+2
+
+def maxTempPerBy = finnMaxTempPerBy(byer, imorgen)
+maxTempPerBy.findAll { 
+    it.value == maxTempPerBy.values().max()
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Oppgave:
+
+Gitt en liste med byer i norge, hvor blir det varmest i morgen?
+
+Tips: Yr har et par tjenester som kan brukes:
+- Url mm. for steder med angitt navn:    http://www.yr.no/_/websvc/jsonforslagsboks.aspx?s=Trondheim
+- Værvarsel kommende døgn for gitt sted: http://www.yr.no/sted/Norge/S%C3%B8r-Tr%C3%B8ndelag/Trondheim/Trondheim/varsel.xml
