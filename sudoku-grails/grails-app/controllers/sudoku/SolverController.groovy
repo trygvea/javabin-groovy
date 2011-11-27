d76df59bf7855974efc88322920ac4fa233bef2b
trygvea <trygve.amundsen@gmail.com>
2011-11-27 22:31:50 +0100

    Added solve and show on SolverController, with test

--- sudoku-grails/grails-app/controllers/sudoku/SolverController.groovy
+++ sudoku-grails/grails-app/controllers/sudoku/SolverController.groovy
@@ -1,6 +1,38 @@
 package sudoku
 
 class SolverController {
+    def solverService
 
-    def index() { }
+    def index() { 
+        redirect action:'show'
+    }
+    
+    def show() {
+        def cells = games[new Random().nextInt(games.size())]
+        [cells:cells]
+    }
+    
+    def solve() {
+        def solution = solverService.solve(params.cells as List)
+        if (!solution) { 
+            flash.message = "Oppgaven er uløselig"
+        }
+        render(view: "show", model: [cells:solution ?: params.cells])
+    }
+    
+    private games = [
+        "   1    5  7 48  2 2 9    7 3   29  5       4  68   1 8    1 4 6  28 5  1    4   ",
+        "   897     9     1  6 1  9  3     2    574    1     6  8  2 7  5     9     763   ",
+        "5   1 9  73 2    5    6  7    5  8  8       3  4  7    1  8    2    1 69  6 7   4",
+        " 7  4  63  2  9 4 5     8      7 3  9  8 6  7  8 5      7     2 1 4  7  69  2  3 ",
+        "57  9 18  3      4 8    6    24 5               6 95    5    9 3      2  91 3  75",
+        " 7  4  63  2  9 4 5     8      7 3  9  8 6  7  8 5      7     2 1 4  7  69  2  3 ",
+        "18    4     8       9 345   4 96    52  8  76    53 1   251 7       2     7    92",
+        " 6  3     459   28  8   73     9  5 9  8 6  7 8  5     36   9  42   938     2  1 ",
+        "  14         786 1    5 9   8     23 13   56 95     7   5 4    3 918         73  ",
+        "7 5 2   3  35     4  7    6    3 82           62 9    3    8  9     41  1   7 3 2",
+        "  1  74      2  966  3      57  89  93     51  69  27      6  582  7      52  8  ",
+        "  73  2  3       18  62     734    5         5    849     67  42       6  9  43  "
+    ]
+    
 }
--- sudoku-grails/test/unit/sudoku/SolverControllerTests.groovy
+++ sudoku-grails/test/unit/sudoku/SolverControllerTests.groovy
@@ -1,17 +1,18 @@
 package sudoku
 
+import grails.test.mixin.TestFor
+
+import org.junit.Test
 
 
-import grails.test.mixin.*
-import org.junit.*
-
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(SolverController)
 class SolverControllerTests {
 
-    void testSomething() {
-       fail "Implement me"
+    @Test
+    void testSolve() {
+        controller.solverService = new SolverService()
+        params.cells = '2  37   9  92    7  1  4  2 5    8    8   9    6    4 9  1  5  8    76  4   89  1' as List
+        controller.solve( )
+        assert model.cells == '284375169639218457571964382152496873348752916796831245967143528813527694425689731' as List
     }
 }
