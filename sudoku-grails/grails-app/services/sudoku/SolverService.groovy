c0baa4326825b54dfd1d2db040d23b88e5a62d7a
trygvea <trygve.amundsen@gmail.com>
2011-11-27 22:29:43 +0100

    Added SolverService with test

--- sudoku-grails/grails-app/services/sudoku/SolverService.groovy
+++ sudoku-grails/grails-app/services/sudoku/SolverService.groovy
@@ -1,8 +1,66 @@
 package sudoku
 
+/**
+ * Slightly modified version of http://www.colloquial.com/games/sudoku/java_sudoku.html
+ */
 class SolverService {
-
-    def serviceMethod() {
-
+    
+    List solve(List cells) {
+        def grid = toGrid(cells)
+        solve(0,0,grid) ? toCells(grid) : []
     }
+    
+    private List<List> toGrid(List cells) {
+        (0..8).collect { i ->
+            (0..8).collect { j->
+                cells[i*9+j]
+            }
+        }
+    }
+    
+    private List toCells(List<List> grid) {
+        (0..80).collect { i ->
+           grid[i.intdiv(9)][i % 9]
+        }
+    }
+
+    private boolean solve(int i, int j, List<List> grid) {
+        if (i == 9) {
+            i = 0
+            if (++j == 9)
+                return true
+        }
+        if (!grid[i][j].allWhitespace)  // skip filled cells
+            return solve(i+1,j,grid)
+
+        for (char val = '1'; val <= '9'; ++val) {
+            if (legal(i,j,val,grid)) {
+                grid[i][j] = val
+                if (solve(i+1,j,grid))
+                    return true
+            }
+        }
+        grid[i][j] = ' ' // reset on backtrack
+        return false
+    }
+
+    private boolean legal(int i, int j, char val, List<List> grid) {
+        for (int k = 0; k < 9; ++k)  // row
+            if (val == grid[k][j])
+                return false
+
+        for (int k = 0; k < 9; ++k) // col
+            if (val == grid[i][k])
+                return false
+
+        int boxRowOffset = ((int)i.intdiv(3))*3;
+        int boxColOffset = ((int)j.intdiv(3))*3;
+        for (int k = 0; k < 3; ++k) // box
+            for (int m = 0; m < 3; ++m)
+                if (val == grid[boxRowOffset+k][boxColOffset+m])
+                    return false
+
+        return true // no violations, so it's legal
+    }
+
 }
--- sudoku-grails/test/unit/sudoku/SolverServiceTests.groovy
+++ sudoku-grails/test/unit/sudoku/SolverServiceTests.groovy
@@ -1,17 +1,16 @@
 package sudoku
 
+import grails.test.mixin.TestFor
+
+import org.junit.Test
 
 
-import grails.test.mixin.*
-import org.junit.*
-
-/**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
 @TestFor(SolverService)
 class SolverServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    @Test
+    void testSolve() {
+        def solution = service.solve( '2  37   9  92    7  1  4  2 5    8    8   9    6    4 9  1  5  8    76  4   89  1' as List)
+        assert solution == '284375169639218457571964382152496873348752916796831245967143528813527694425689731' as List
     }
 }
